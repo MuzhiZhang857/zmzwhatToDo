dde50361909312914829e6935975b33f16baf27f
MuzhiZhang857 <15561277227@163.com>
2026-01-08 16:29:50 +0800

    Configure admin user management

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = (
+        "id",
+        "email",
+        "username",
+        "name",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = ("is_staff", "is_active", "is_superuser", "groups")
+    search_fields = ("email", "username", "name")
+    ordering = ("id",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("username", "name", "first_name", "last_name")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        if request.user.is_superuser:
+            return queryset
+        return queryset.filter(is_superuser=False)
+
+    def has_change_permission(self, request, obj=None):
+        if obj and obj.is_superuser and not request.user.is_superuser:
+            return False
+        return super().has_change_permission(request, obj=obj)
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = list(super().get_readonly_fields(request, obj=obj))
+        if request.user.is_superuser:
+            return readonly_fields
+        readonly_fields.extend(["is_superuser", "groups", "user_permissions"])
+        return readonly_fields
